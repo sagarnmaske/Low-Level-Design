faca6c8ff75c5b1871ca0291caf345af3b2f7f7f
sagarnmaske <sagarnmaske@gmail.com>
2026-03-23 23:12:34 +0530

    executive service with callable future

---
+++ src/executorService/ParallelTaskController.java
@@ -0,0 +1,79 @@
+package executorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ParallelTaskController {
+    public void parallelTask() {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+       Future<String> s1 = executor.submit(() -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(Thread.currentThread().getName());
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                sb.append(i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return sb.toString();
+        });
+        Future<String> s2 = executor.submit(() -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(Thread.currentThread().getName());
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                sb.append(i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return sb.toString();
+        });
+        Future<String> s4 = executor.submit(() -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(Thread.currentThread().getName());
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                sb.append(i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return sb.toString();
+        });
+        Future<String> s3 = executor.submit(() -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(Thread.currentThread().getName());
+            for (int i = 0; i < 100; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                sb.append(i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return sb.toString();
+        });
+
+        try{
+            System.out.println(s1.get());
+            System.out.println(s2.get());
+            System.out.println(s3.get());
+            System.out.println(s4.get());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        executor.shutdown();
+    }
+}
